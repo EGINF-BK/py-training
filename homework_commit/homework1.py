0f4efd6661825d4b4c947bf8644c1cac8d185b73
孙成阳 <969391146@qq.com>
2018-10-11 14:39:10 +0800

    update

--- homework_commit/homework1.py
+++ homework_commit/homework1.py
@@ -1,57 +1,60 @@
 # -*- coding=utf-8 -*-
+from os import path
 
-url = r'D:\Users\010500571\Desktop\py-training\homework\access.log.txt'
+file_name = 'access.log.txt'
+current_name = 'homework1'
 
-def open_file(url):
+def load_file_path(file_name):
+    current_path = path.abspath(current_name)
+    father_path = path.abspath(path.dirname(current_path)+path.sep+'..')
+    file_path = 'homework\\'+file_name
+    return path.join(father_path,file_path)
+
+
+file_path = load_file_path(file_name)
+
+
+def open_file(file_path):
     total_list = []
-    f = open(url,'r')
+    f = open(file_path,'r')
     data = f.readlines()
     f.close()
     for line in data:
         line = line.strip('\n')
         total_list.append(line.split(' '))
-    #for i in range(len(total_list[0])):
-    #    print total_list[0][i],i
     return total_list
 
-#open_file(url)
 
-def calculate_pv_uv():
-    data = open_file(url)
-    pv = []
-    uv = []
-    for item in data:
-        pv.append(item[0])
-    pv_count = len(pv)
-    print pv_count
-    uv = set(pv)
-    uv_count = len(uv)
-    print uv_count
+data = open_file(file_path)
 
-def top_resource(url,num):
-    data = open_file(url)
-    url_dict = {}
-    dict_sorted = {}
-    url_list = []
-    for item in data:
-        item = item[10].strip('"')
-        if item != '-':
-            url_list.append(item)
-    for item in set(url_list):
-        url_dict.setdefault(item, 1)
-    for item in url_list:
-        if url_dict.get(item):
-            url_dict[item] += 1
 
-    dict_sorted = sorted(url_dict.items(),key=lambda e:e[1],reverse = True)
+def calculate_pv_uv(file_path):
+    data = open_file(file_path)
+    pv = [item[0] for item in data]
+    print 'PV:',len(pv)
+    print 'UV:',len(set(pv))
 
+
+def top_resource(file_path,num):
+    data = open_file(file_path)
+    resource_dict = {}
+    resource_total = []
+    for i in range(len(data)):
+        resource_single = [x for x in data[i][6].split('/') if x != '']
+        if resource_single:
+            resource_total.append(resource_single[0])
+    for item in set(resource_total):
+        resource_dict.setdefault(item, 1)
+    for item in resource_total:
+        if resource_dict.get(item):
+            resource_dict[item] += 1
+    dict_sorted = sorted(resource_dict.items(),key=lambda e:e[1],reverse = True)
     for i in range(num):
         print dict_sorted[i][0],':',dict_sorted[i][1]
 
-top_resource(url,5)
 
-def web_availabe_rate(url):
-    data = open_file(url)
+def web_availabe_rate(file_path):
+    data = open_file(file_path)
     url_list = []
     availabe_count = 0
     unavailabe_count = 0
@@ -62,12 +65,16 @@ def web_availabe_rate(url):
             availabe_count += 1
         else:
             unavailabe_count += 1
-    #print availabe_count,unavailabe_count
     total_count = len(url_list)
     availabe_rate = float(availabe_count)/total_count
     unavailabe_rate = float(unavailabe_count)/total_count
     print repr(round(availabe_rate*100,2))+'%',repr(round(unavailabe_rate*100,2))+'%'
     return availabe_rate,unavailabe_rate
 
-#web_availabe_rate(url)
 
+def main(file_path):
+    calculate_pv_uv(file_path)
+    top_resource(file_path, 5)
+    web_availabe_rate(file_path)
+
+main(file_path)
--- homework_commit/homework2.py
+++ homework_commit/homework2.py
@@ -4,20 +4,15 @@ from os import path
 
 file_path = r'c:\windows\system32'
 
-#file_name = os.listdir(file_path)
-#for name in file_name:
-    #print name
-
 def getdirsize(file_path):
     size = 0
     for root,dirs,files in os.walk(file_path):
         size += sum(path.getsize(path.join(root,name)) for name in files)
     return size
-#print getdirsize(file_path)
+
 
 def listdir(file_path):
     for root,dirs,files in os.walk(file_path):
-        #print root
         for file in files:
             size_file = getdirsize(path.join(root, file))
             if round(float(size_file) / (1024 * 1024), 2) > 200:
@@ -25,5 +20,6 @@ def listdir(file_path):
         size_dir = getdirsize(root)
         if round(float(size_dir)/(1024*1024),2) > 200:
             print root,str(round(float(size_dir)/(1024*1024),2))+'MB'
+
 listdir(file_path)
 
