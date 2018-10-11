944f7aa813cfbb724cf65e57f4289f2b94c50a25
719899089@qq.com <081205132@hdjt.hdad.local>
2018-10-11 16:39:00 +0800

    ignore

--- liutong/venv/log.py
+++ liutong/venv/log.py
@@ -5,7 +5,7 @@ import string
 
 s=open(r"D:\Users\081205132\Desktop\py-training\homework\access.log.txt","rb").read()
 results=re.findall("(?isu)(http\://[a-zA-Z0-9\.\?/&\=\:]+)",s)
-open("urls.txt", "w").write("".join(results) + "\r\n")
+open("get.txt", "w").write("".join(results) + "\r\n")
 from collections import  Counter
 print Counter(str(i) for i in results)
 print 'finished'
