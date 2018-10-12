846bb51abf10c8d3d3ce79f499379fdaecd7aa04
孙成阳 <969391146@qq.com>
2018-10-12 09:11:41 +0800

    update homework2

--- homework_commit/homework2.py
+++ homework_commit/homework2.py
@@ -2,7 +2,7 @@
 import os
 from os import path
 
-file_path = r'c:\windows\system32'
+file_path = path.join('c:',path.sep,'windows','system32')
 
 def getdirsize(file_path):
     size = 0
@@ -11,15 +11,15 @@ def getdirsize(file_path):
     return size
 
 
-def listdir(file_path):
+def listdir(file_path,size):
     for root,dirs,files in os.walk(file_path):
         for file in files:
             size_file = getdirsize(path.join(root, file))
-            if round(float(size_file) / (1024 * 1024), 2) > 200:
+            if round(float(size_file) / (1024 * 1024), 2) > size:
                 print path.join(root, file), str(round(float(size_dir) / (1024 * 1024), 2)) + 'MB'
         size_dir = getdirsize(root)
-        if round(float(size_dir)/(1024*1024),2) > 200:
+        if round(float(size_dir)/(1024*1024),2) > size:
             print root,str(round(float(size_dir)/(1024*1024),2))+'MB'
 
-listdir(file_path)
+listdir(file_path,200)
 
